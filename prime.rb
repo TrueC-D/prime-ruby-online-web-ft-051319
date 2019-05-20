7662b44c03a74c7e1d9ffeccd87d82442695c60b
Alethea Cary <aletheacary@gmail.com>
2019-05-20 04:03:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(x)
+  if x == 2
+    return true 
+  elsif x < 2
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
+end
